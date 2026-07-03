ab3b4f7905bb571b2370f104bcc4eb3b7a7895fd
Dheeraj Kumar <dheerajkumar.cse@gmail.com>
2026-07-03 12:17:50 +0000

    dataypes

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,9 @@
+const score=100;
+const scoreValue=100.3
+const isLoggedIn=false //boolean
+const outSideTemp=null
+let userEmail;
+const bigInt=416464641641646464646464646464646464679769464;
+
+const myInfo = { fName:"dheeraj", lName:"kumar" };
+console.log(typeof myInfo);
