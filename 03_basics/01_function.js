b858da712ff370b67f6de870496d6038b1fdafc4
Dheeraj Kumar <dheerajkumar.cse@gmail.com>
2026-07-10 14:13:05 +0000

    function_next

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,5 +1,22 @@
-function  addTwoNumbers(number1,number2){
+// function  addTwoNumbers(number1,number2){
    
-    return number1+number2;
+//     return number1+number2;
+// }
+// console.log(addTwoNumbers(3,5));
+
+
+// function calculateCartPrice(...num1){
+
+//     return num1
+// }
+// console.log(calculateCartPrice(200,300,400))
+
+const user={
+        username:"dheeraj",
+        price:199
 }
-console.log(addTwoNumbers(3,5));
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
