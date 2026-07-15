5fdef72096e3b8ebefa10c0ee81e39cc62be6708
Dheeraj Kumar <dheerajkumar.cse@gmail.com>
2026-07-15 17:45:56 +0000

    foreach

---
+++ 05_iterations/five.js
@@ -0,0 +1,6 @@
+const coding=['js','ruby', 'java', 'python'];
+
+coding.forEach((item,indes,array)=>{
+
+    console.log(item,indes,array)
+})
---
+++ 05_iterations/three.js
@@ -0,0 +1,27 @@
+//for of 
+// const arr= [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num)
+// }
+
+// const greetings="hello world"
+// for (const num of greetings) {
+//     console.log(num)
+// }
+
+// const map= new Map();
+
+const myObject={
+    js: "javascript",
+    cpp: "c++",
+    cp: "c",
+    rb: "ruby"
+
+}
+for (const key in myObject) {
+   
+    
+    console.log(myObject[key])
+}
+ 
