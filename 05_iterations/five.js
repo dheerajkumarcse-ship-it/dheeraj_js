c743e9c8f2059948fc6a67e92d70b88b5e9b9e22
Dheeraj Kumar <dheerajkumar.cse@gmail.com>
2026-07-15 18:45:42 +0000

    reduce done

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,6 +1,24 @@
-const coding=['js','ruby', 'java', 'python'];
+// const coding=['js','ruby', 'java', 'python'];
 
-coding.forEach((item,indes,array)=>{
+// // coding.forEach((item,indes,array)=>{
 
-    console.log(item,indes,array)
-})
+// //     console.log(item,indes,array)
+// // })
+
+// const a=[];
+// a.push(coding);
+// console.log(...a)
+
+// const myNumbers=[1,2,3,4,5,6,7,8,9,10];
+
+// // const newNum=myNumbers.map((num)=>num+10);
+
+
+// console.log(newNum)
+
+const myNums=[1,2,3];
+
+const myTotal=myNums.reduce((acc,curr)=>acc+curr,0)
+
+console.log(myTotal);
+
