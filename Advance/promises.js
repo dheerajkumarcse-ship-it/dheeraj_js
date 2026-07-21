4a84caa69c9ebd5cbb3ba7d7c5660f88e0465346
Dheeraj Kumar <dheerajkumar.cse@gmail.com>
2026-07-21 17:56:37 +0000

    promise

---
+++ Advance/promises.js
@@ -0,0 +1,58 @@
+// const promiseOne = new Promise(function(resolve,){
+//     //db calls, cryptography, network call, 
+//     setTimeout(function()
+//     {
+//        console.log('Async task is complete') 
+//        resolve()
+//     }, 1000)
+// });
+
+// promiseOne.then(function(){
+//     console.log("Promise consumed")
+// })
+
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("Sync task 2")
+//             resolve()
+        
+//     },1000)
+// }).then(function(){
+//     console.log("Asyc 2 Resolved")
+// })
+
+// const PromiseThree= new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         resolve({username:"dheeraj", email:"dheerajkr@gmail.com"})
+
+//     },1000)
+// })
+
+// PromiseThree.then(function(user){
+//     console.log(user)
+
+// })
+
+const promiseFour= new Promise(function(resolve,reject){
+
+    setTimeout(function(){
+
+        let error=true;
+        if(!error)
+        {resolve({username:"dheeraj", password:"123"})}
+        else{
+            reject("Error! something went wrong")
+        }
+    },1000)
+})
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+console.log(username)
+    
+}).catch(function(error){
+    console.log(error)
+}).finally(()=>{
+    console.log("the promise is either resove or refected")
+})
